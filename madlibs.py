1bfa65fcf2c73740a2fd4752cbaf21e4ff232465
Boomingcrypto12345 <157722654+Boomingcrypto12345@users.noreply.github.com>
2024-01-29 15:31:25 +0000

    done

--- madlibs.py
+++ madlibs.py
@@ -1,1 +1,22 @@
 
+adverb = input("adverb? ")
+
+bannana_color = input("bannana color? ")
+
+sport = input("Sport? ")
+
+tree = input("tree type? ")
+
+animal = input("animal? ")
+
+country = input("country? ")
+
+food = input("food? ")
+
+famousperson = input("famous person? ")
+
+verb = input("verb? ")
+
+ocean = input("ocean? ")
+
+print (f"I {adverb} ate {bannana_color} bannanas after a {sport} game under a {tree} tree. A {animal} is now chasing me across {country} I think he wants my {food}.I hope {famousperson} saves me while I {verb} across the {ocean} ocean.")
